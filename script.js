d0f9a50f7cf40456e97a981b24dc2cb95bba6fc9
h21jonth <jonatanthornell@gmail.com>
2022-11-20 22:08:53 +0100

    Genererar html-lista

--- script.js
+++ script.js
@@ -15,7 +15,6 @@ const bookList = [
 ];
 /*const searchField = document.children[0].children[1].children[1].children[1];*/
 const searchField = document.getElementById("searchField");
-console.log(searchField);
 
 searchField.addEventListener("keyup", handleKeyPress);
 
@@ -43,7 +42,27 @@ function searchBooks(searchTerm) {
     renderBooklist(filteredList);
 }
 
-function renderBooklist(list) {
+function renderBooklist(bookList) {
     /*Element i HTML-listan visas eller döljs beroende på listans innehåll. */
-    console.log(list)
+    let html = `<ul class="book-list rounded-md border-2 border-blue-400 bg-white w-full mx-auto">`
+    for(let i = 0; i < bookList.length; i++) {
+        html += `<li 
+                        class="book-list__item mb-2 mx-2 last:mb-0 p-3 text-indigo-900 last:border-b-0 border-b
+                        border-indigo-700 cursor-pointer">
+                    ${bookList[i].author} - ${bookList[i].title}  
+                    </li>`
+    }                
+    html += `</ul>`;
+
+    const existingElement = document.querySelector(".book-list");
+    console.log(existingElement);
+
+    const root = document.getElementById("root");
+    if(existingElement){
+        root.removeChild(existingElement);
+    }
+
+    root.insertAdjacentHTML("beforeend", html);
+
+
 }
